f589e62191aa0d61976864e28098da3443a787d0
vijay-ram365 <ramkissoon.vijay@gmail.com>
2021-12-03 16:01:36 -0500

    removed button

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
-let button = document.getElementById("button1");
 
 function setGradient() {
   body.style.background =
